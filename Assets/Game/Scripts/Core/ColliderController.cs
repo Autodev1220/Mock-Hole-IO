c47af0f0b9daffe5a13d7b643a8c9f9818ddbc49
allanv <allanjay.valdez1220@gmail.com>
2022-03-13 01:34:45 +0800

    Implements generating multi-hole in mesh collider

--- Assets/Game/Scripts/Core/ColliderController.cs
+++ Assets/Game/Scripts/Core/ColliderController.cs
@@ -6,43 +6,48 @@ using UnityEngine;
 namespace Game.Core{
     public class ColliderController : MonoBehaviour
     {
-        [SerializeField] PolygonCollider2D holePoly2d;
+        [SerializeField] PolygonCollider2D[] holePoly2ds;
         [SerializeField] PolygonCollider2D polyGroundCollider;
         [SerializeField] MeshCollider GroundCollider;
-        [SerializeField] GameObject player;
-
-
+        [SerializeField] GameObject[] players;
         [SerializeField]Mesh GeneratedMesh;
 
-        private void Start()
-        {
-            if(player == null) player = GameObject.FindGameObjectWithTag("Player");
+        private void FixedUpdate() {
+                holeMovements(); //check position of every hole per fixed update and genrate hole to mesh collider
+                GenerateMeshCollider();
+        }
+
+        void holeMovements(){
+            for (int i = 0; i < players.Length; i++)
+            {
+                holePoly2ds[i].gameObject.transform.position = new Vector2(players[i].transform.position.x, players[i].transform.position.z);
+            }
+            GenerateHole();
             
         }
 
-        private void FixedUpdate() {
-            if(player.transform.hasChanged == true){
-                player.transform.hasChanged = false;
-                holePoly2d.gameObject.transform.position = new Vector2(player.transform.position.x, player.transform.position.z);
-                holePoly2d.transform.localScale = player.transform.localScale * .5f;
-                GenerateHole();
-                GenerateMeshCollider();
-            }
-            
-           
-        }
+        
 
         void GenerateHole()
         {
-            Vector2[] pointsPositions = holePoly2d.GetPath(0);
+           polyGroundCollider.pathCount = players.Length + 1; // Get how many players
+           for(int p = 0; p < players.Length; p++){
+                Vector2[] pointsPositions = holePoly2ds[p].GetPath(0); // get the initial path of the polygon collider
+                for (int index = 0; index < pointsPositions.Length; index++)
+                {
+                    //pointsPositions[index] = (Vector2)holePoly2d.transform.position;
+                    pointsPositions[index] = holePoly2ds[p].transform.TransformPoint(pointsPositions[index]);
+                }
+                polyGroundCollider.SetPath(p+1, pointsPositions); //set path and pointPosition for the ground collider to generate a hole in mesh collider
+           }
+            
 
-            for(int index = 0; index < pointsPositions.Length;index++){
-               //pointsPositions[index] = (Vector2)holePoly2d.transform.position;
-               pointsPositions[index] = holePoly2d.transform.TransformPoint(pointsPositions[index]);
-            }
+            // = to length of players
+            //iterate setpath
+            
 
-            polyGroundCollider.pathCount = 2;
-            polyGroundCollider.SetPath(1,pointsPositions);
+            
+            
         }
 
         void GenerateMeshCollider(){
